0cc14c7a5bbdccaa60e2e899affed6231471910a
Paweł Górecki <gorecki@mimuw.edu.pl>
2026-03-05 14:19:49 +0100

    Initial files

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
+
 # NetEC-Simulations
+
+This datasets shows inference of WGD events from simulatated WGD scenarios using NetEC, a tool for inferring whole genome duplication (WGD) episodes by reconciling gene trees with species trees or networks. It uses dynamic programming to find the minimum number of duplication episodes that explain the observed gene tree duplications.
+
+## Requirements
+
+Download the NetEC repository:
+
+```bash
+git clone git@github.com:ppgorecki/netec.git
+```
+
+## Input Files
+
+A simulated network and 7 sets of gene trees.
+
+- **Network:** `data/net.nwk`
+- **Gene trees:** `data/*.gtrees`
+
+## Results 
+
+The output files by netec (execpt .log) are stored in replicate_1 directory.
+
+## Usage
+
+Execute `run.sh` to start netec analysis. It will overwrite contents of replicate_1 directory.
+
+- **The first WGD analysis**
+
+```bash
+run.sh replicate_1
+```
+
+- ** Continue analysis with fixed WGD at node B
+```bash
+run.sh replicate_1 fixB
+```
---
+++ run.sh
@@ -0,0 +1,41 @@
+
+# A - 15
+# B - 27
+# C - 3
+
+if ! [[ $1 ]]
+then
+   cat << EOF 
+Usage:
+	$0 [fixB] [DIRPREFIX]
+
+Requirements:
+	netec repo in the main dir
+
+Examples:
+	first run: $0 replicate_1
+	second run: $0 fixB replicate_1
+EOF
+   exit -1
+fi
+
+if ! [[ -f netec/netec.py ]]
+then
+
+	echo "Download netec first: "	
+	echo "git clone git@github.com:ppgorecki/netec.git"
+	exit -1
+fi
+
+VARIANTS="wgdac wgdab wgdbc wgdb wgda wgdc nowgd"
+
+if [[ $1 -eq fixB ]]
+then
+	shift
+	# add 27 and continue
+	parallel --ungroup -j 4 netec/netec.py --network data/net.nwk --gene_trees data/{1}.nolabelsuffix.gtrees --extended_episodes_search --out_dir "$1"_fixB/{1} --fixed_episodes "27" --gsestyle --verbose 4 --locked_epi_support --save_embedding ::: $VARIANTS
+else
+	parallel --ungroup -j 4 netec/netec.py --network data/net.nwk --gene_trees data/{1}.nolabelsuffix.gtrees --extended_episodes_search --out_dir $1/{1} --verbose 2 --gsestyle --locked_epi_support --save_embedding ::: $VARIANTS
+fi
+
+
